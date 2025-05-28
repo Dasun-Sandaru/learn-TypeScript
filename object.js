41967bfcae21665a2794038747dba8aeba6a3931
Dasun-Sandaru <105912727+Dasun-Sandaru@users.noreply.github.com>
2025-05-28 06:50:26 +0530

    objects

---
+++ object.js
@@ -0,0 +1,16 @@
+"use strict";
+let person1 = {
+    name: "Dasun",
+    age: 27,
+    isStudent: true,
+};
+let person2 = {
+    name: "John",
+    age: 30,
+    isStudent: false,
+};
+let person3 = {
+    name: "Alice",
+    age: 25,
+    isStudent: true,
+};
---
+++ object.ts
@@ -0,0 +1,24 @@
+let person1: {name: string, age: number, isStudent: boolean} = {
+  name: "Dasun",
+  age: 27,
+  isStudent: true,
+}
+
+let person2: {name: string, age: number, isStudent: boolean} = {
+  name: "John",
+  age: 30,
+  isStudent: false,
+};
+
+// custom types
+type Person = {
+  name: string;
+  age: number;
+  isStudent: boolean;
+};
+
+let person3: Person = {
+  name: "Alice",
+  age: 25,
+  isStudent: true,
+};
