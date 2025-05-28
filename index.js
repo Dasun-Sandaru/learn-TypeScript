e26415f3e878a5fd398b9cdf096a5a097a828bd1
Dasun-Sandaru <105912727+Dasun-Sandaru@users.noreply.github.com>
2025-05-28 06:50:11 +0530

    primitive types & arrays

---
+++ index.js
@@ -0,0 +1,18 @@
+"use strict";
+// let myName = "Dasun";
+let myName = "Dasun";
+let myAge = 27;
+let isStudent = true;
+console.log(`My name is ${myName}, I am ${myAge} years old, and it is ${isStudent} that I am a student.`);
+let myUndifinedVar = undefined;
+let myNullVar = null;
+let myNumberArray = [1, 2, 3, 4, 5];
+let myStringArray = ["apple", "banana", "cherry"];
+let myMixedArray = [1, "apple", 2, "banana"];
+let arrofArrays = [
+    [1, "apple"],
+    [2, "banana"],
+    [3, "cherry"],
+];
+let fixedArray1 = ["apple", "banana"];
+let fixedArray2 = ["apple", 3];
---
+++ index.ts
@@ -0,0 +1,24 @@
+// let myName = "Dasun";
+let myName: string = "Dasun";
+let myAge: number = 27;
+let isStudent: boolean = true;
+console.log(`My name is ${myName}, I am ${myAge} years old, and it is ${isStudent} that I am a student.`);
+
+let myUndifinedVar: undefined = undefined;
+let myNullVar: null = null;
+
+let myNumberArray: number[] = [1, 2, 3, 4, 5];
+let myStringArray: string[] = ["apple", "banana", "cherry"];
+
+let myMixedArray: (number | string)[] = [1, "apple", 2, "banana"];
+
+let arrofArrays: (number | string)[][] = [
+  [1, "apple"],
+  [2, "banana"],
+  [3, "cherry"], 
+];
+
+
+let fixedArray1: [string,string] = ["apple", "banana"];
+let fixedArray2: [string,number] = ["apple", 3];
+
